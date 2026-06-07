ef6d991394413c152a77d14ec957e139e613cf69
khoatran98tx-Sovereign <khoatran98tx@gmail.com>
2026-06-07 18:32:27 -0500

    Update AvatarViewer.js

--- app/components/AvatarViewer.js
+++ app/components/AvatarViewer.js
@@ -1,24 +1,32 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Box } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 function Avatar() {
   return (
     <mesh rotation={[0.5, 0.5, 0]}>
       <boxGeometry args={[1, 2, 1]} />
-      <meshStandardMaterial />
+      <meshStandardMaterial color="orange" />
     </mesh>
   );
 }
 
 export default function AvatarViewer() {
   return (
-    <div style={{ width: 300, height: 300 }}>
+    <div
+      style={{
+        width: "300px",
+        height: "300px",
+        margin: "20px auto"
+      }}
+    >
       <Canvas camera={{ position: [0, 0, 5] }}>
-        <ambientLight />
+        <ambientLight intensity={2} />
         <directionalLight position={[2, 2, 2]} />
+
         <Avatar />
+
         <OrbitControls />
       </Canvas>
     </div>
